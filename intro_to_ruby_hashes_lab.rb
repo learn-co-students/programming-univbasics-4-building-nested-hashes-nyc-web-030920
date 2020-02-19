3fb408c919dd9dcd6a1556bf36d3a356acbf15f2
Andrew de la Vega <andrew21delavega@gmail.com>
2020-02-19 15:51:35 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,17 +16,17 @@ def monopoly_with_third_tier
 	bare_hash = {
     :railroads => {
       :pieces => 4,
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
+      },
       :names => {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline_railroad => {}
-      },
-      rent_in_dollars: {
-        :one_piece_owned => 25,
-        :two_pieces_owned => 50,
-        :three_pieces_owned => 100,
-        :four_pieces_owned => 200
       }
     }
   }
@@ -36,6 +36,12 @@ def monopoly_with_fourth_tier
   base_hash = {
     :railroads => {
       :pieces => 4,
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200
+      },
       :names => {
         :reading_railroad => {
           "mortgage_value" => "$100"
@@ -49,12 +55,6 @@ def monopoly_with_fourth_tier
           :shortline_railroad => {
             "mortgage_value" => "$800"
           }
-      },
-      rent_in_dollars: {
-        :one_piece_owned => 25,
-        :two_pieces_owned => 50,
-        :three_pieces_owned => 100,
-        :four_pieces_owned => 200
       }
     }
   }
