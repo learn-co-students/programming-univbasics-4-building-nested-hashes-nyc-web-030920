1ad2ee105657d62fc36dd4c0e55665ec840e8e3a
Zhiyuan Liu <cloudnotes.64@gmail.com>
2020-03-04 18:05:39 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,26 +1,41 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	{}
+  {
+    :railroads => {}
+    :railroads
+  }
+  
 end
 
 def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  base_hash[:railroads] = {:pieces => 4} 
+  base_hash
+  
 end
 
 def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  
+  monopoly_with_second_tier[:railroads][:rent_in_dollars] = 
+  {
+    :one_piece_owed => 25,
+    :two_pieces_owed => 50,
+    :three_pieces_owed => 100,
+    :four_pieces_owed => 200
+  }
+  base_hash[:railroads][:names] = 
+  {
+    :reading_railroad => {},
+    :pennsylvania_railroad => {},
+    :b_and_o_railroad => {},
+    :shortline_railroad => {}
+    monopoly_with_second_tier
+  }
 end
 
 def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
+  monopoly_with_third_tier
+  [:railroads][:names][:reading_railroad]["mortgage_value"] => "$100"
+  [:railroads][:names][:pennsylvania_railroad]["mortgage_value"] => "$200"
+   [:railroads][:names][:b_and_o_railroad_railroad]["mortgage_value"] => "$400"
+    [:railroads][:names][:shortline_railroad_railroad]["mortgage_value"] => "$800"
+    monopoly_with_third_tier
 end
